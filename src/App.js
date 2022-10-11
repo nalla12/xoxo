eb2297f042a89e564c599735e02e2e6ecd40698a
Allan Duong <allan@proventic.dk>
2022-10-11 20:46:52 +0200

    Updated packages. Config changes. Renamed CSS files according to Tailwind. Added .eslintrc

--- src/App.js
+++ src/App.js
@@ -1,60 +1,15 @@
 import './App.css';
-import {useState} from "react";
-import {Col, Grid, Row} from "@zendeskgarden/react-grid";
+import {Col, Grid, Row} from '@zendeskgarden/react-grid';
 
 function App() {
-    const [bla, setBla] = useState(false);
-    const Value = () => {
-        return (bla ? 'x' : 'o');
-    };
-    const handleClick = () => {
-        setBla(!bla);
-        console.log(bla)
-    };
+
 
     return (
         <div className="App">
             <Grid>
                 <Row>
                     <Col>
-                        <button onClick={handleClick}
-                                className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400"><Value /></button>
-                    </Col>
-                    <Col>
-                        <button onClick={handleClick}
-                                className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400"><Value /></button>
-                    </Col>
-                    <Col>
-                        <button onClick={handleClick}
-                                className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400"><Value /></button>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <button onClick={handleClick}
-                                className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400"><Value /></button>
-                    </Col>
-                    <Col>
-                        <button onClick={handleClick}
-                                className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400"><Value /></button>
-                    </Col>
-                    <Col>
-                        <button onClick={handleClick}
-                                className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400"><Value /></button>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <button onClick={handleClick}
-                                className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400"><Value /></button>
-                    </Col>
-                    <Col>
-                        <button onClick={handleClick}
-                                className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400"><Value /></button>
-                    </Col>
-                    <Col>
-                        <button onClick={handleClick}
-                                className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400"><Value /></button>
+                        <button className="h-20 w-full bg-amber-300 border-2 border-solid border-amber-400">Hej</button>
                     </Col>
                 </Row>
             </Grid>
